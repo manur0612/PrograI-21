45ad68d0ec19ea7d28b9ce24a4ce007c0a274115
manur0612 <manur.0612@gmail.com>
2021-04-27 16:28:36 -0500

    Tareas

---
+++ pokemon/Models/Pokemon.cs
@@ -0,0 +1,9 @@
+namespace pokedex.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Foto { get; set; }
+    }
+}
