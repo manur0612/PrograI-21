894205121e74e5d0f99a440bf6e0de11e1c24231
manur0612 <manur.0612@gmail.com>
2021-04-27 18:15:14 -0500

    Carga inicial

---
+++ appLaPiranha/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace appLaPiranha.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- pokedex-master/Models/Pokemon.cs
+++
@@ -1,9 +0,0 @@
-namespace pokedex.Models
-{
-    public class Pokemon
-    {
-        public int Id { get; set; }
-        public string Nombre { get; set; }
-        public string Foto { get; set; }
-    }
-}
