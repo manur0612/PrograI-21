4bc78fb4e3a46163b4f8cc28a8ba2f9e5f201972
Manuel Ríos <manur.0612@gmail.com>
2021-05-25 14:31:25 -0500

    Prueba

---
+++ appPrueba/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace appPrueba.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
